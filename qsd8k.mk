82e631cc2abc1df0d0a4a2e2b8ec9729c9416845
Wolfgang Hobmaier <0woh0.android@gmail.com>
2013-07-31 08:43:00 +0200

    Revert "qsd8k: use gpu for screen composition"
    No longer needed because ION will be reverted aswell
    
    This reverts commit 79b7e55bfe0136fa3d6e9dcba03301742613ddeb.

--- qsd8k.mk
+++ qsd8k.mk
@@ -75,7 +75,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.hw=1 \
     debug.hwc.fakevsync=1 \
-    debug.composition.type=gpu \
+    debug.composition.type=mdp \
     debug.gr.numframebuffers=2
 
 #
